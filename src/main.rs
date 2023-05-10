9356b36596d3698b555d48e1ed99cbc1925d9068
Jeffrey Lemoine <jeffmlife@gmail.com>
2023-05-09 20:58:22 -0700

    loop until no bytes read

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,28 @@ fn main() {
 
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
+    let mut n = 1;
     for stream in listener.incoming() {
+        n += 1;
+        println!("{}", n);
+
         match stream {
             Ok(mut _stream) => {
-                // Array with a fixed size
-                let mut rx_bytes = [0u8; MESSAGE_SIZE];
-                // Read from the current data in the TcpStream
-                _stream.read(&mut rx_bytes).expect("read err");
-                // Write PONG no matter what
-                _stream.write(b"+PONG\r\n").expect("write err");
+                loop {
+                    println!("Recieved message");
+                    // Array with a fixed size
+                    let mut rx_bytes = [0u8; MESSAGE_SIZE];
+                    // Read from the current data in the TcpStream
+                    let bytes_read = _stream.read(&mut rx_bytes).expect("read err");
+                    println!("bytes read: {}", bytes_read);
+                    if bytes_read == 0 {
+                        break;
+                    }
+                    let received = std::str::from_utf8(&rx_bytes).expect("valid utf8");
+                    println!("{}", received);
+
+                    _stream.write(b"+PONG\r\n").expect("write err");
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
